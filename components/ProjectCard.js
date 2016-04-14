83b723470ba7c6cde7a284e54c9b700c4f12266a
Rowell Heria <owieh@me.com>
2016-04-15 00:42:15 +0100

    ProjectCard component added. Comes with a placeholder image.

---
+++ components/ProjectCard.js
@@ -0,0 +1,40 @@
+import React, {PropTypes} from 'react';
+import { Link } from 'react-router';
+
+const ProjectCard = ({ link, title, image, containerStyle }) =>
+ <Link to={link} style={{ ...styles.projectCard, ...containerStyle }}>
+   <div className="image" style={{ background: `url(${image || 'images/placeholder.png'}) center center`, ...styles.projectImage }}></div>
+   <div className="title" style={styles.projectTitle}>
+     {title}
+   </div>
+ </Link>;
+
+ProjectCard.propTypes = {
+  link: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string
+};
+
+const styles = {
+  projectCard: {
+    display: 'block',
+    borderRadius: 2,
+    background: '#ffffff',
+    boxShadow: '0 2px 5px 0 rgba(0,0,0,.16),0 2px 10px 0 rgba(0,0,0,.12)',
+    textDecoration: 'none'
+  },
+  projectImage: {
+    backgroundSize: 'cover',
+    borderTopRightRadius: 2,
+    borderTopLeftRadius: 2,
+    height: 200
+  },
+  projectTitle: {
+    padding: 5,
+    fontFamily: 'sans-serif',
+    color: '#677c91',
+    textDecoration: 'none'
+  }
+};
+
+export default ProjectCard;
