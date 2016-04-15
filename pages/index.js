c6958943572871501768170a054772839fbc544a
Rowell Heria <owieh@me.com>
2016-04-16 00:14:58 +0100

    PorjectCard now points to timetabled

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default class Index extends React.Component {
           </Container>
         </Grid>
       </DocumentTitle>
-    )
+    );
   }
 }
 
--- pages/projects.js
+++ pages/projects.js
@@ -16,7 +16,7 @@ const Projects = () =>
           <h2>Projects</h2>
         </Span>
         <Span columns={3}>
-          <ProjectCard link="/projects/sample/" title="Sample" />
+          <ProjectCard link="/projects/timetabled/" title="timetabled" />
         </Span>
         <Span columns={3}>
           <ProjectCard link="/projects/sample/" title="Sample" />
