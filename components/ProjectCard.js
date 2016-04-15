913a52e4f56503af3719edc5df825e373aefadbd
Rowell Heria <owieh@me.com>
2016-04-15 01:16:46 +0100

    ProjectCard now has a state so we can do hover effect. Still not sure if it is worth it :P

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,19 +1,41 @@
 import React, {PropTypes} from 'react';
 import { Link } from 'react-router';
 
-const ProjectCard = ({ link, title, image, containerStyle }) =>
- <Link to={link} style={{ ...styles.projectCard, ...containerStyle }}>
-   <div className="image" style={{ background: `url(${image || 'images/placeholder.png'}) center center`, ...styles.projectImage }}></div>
-   <div className="title" style={styles.projectTitle}>
-     {title}
-   </div>
- </Link>;
+export class ProjectCard extends React.Component {
+  static propTypes = {
+    link: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string
+  };
 
-ProjectCard.propTypes = {
-  link: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  image: PropTypes.string
-};
+  componentWillMount () {
+    this.setState({
+      hover: false
+    });
+  }
+
+  hoverToggle () {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+
+  render() {
+    let {link, title, image, containerStyle} = this.props;
+    let hoverStyles = this.state.hover ? styles.projectCardHover : {};
+
+    return (
+      <Link to={link} style={{ ...styles.projectCard, ...containerStyle, ...hoverStyles }} onMouseEnter={() => this.hoverToggle()}
+        onMouseLeave={() => this.hoverToggle()} onFocus={() => this.hoverToggle()} onBlur={() => this.hoverToggle()} >
+        <div className="image"
+             style={{ background: `url(${image || 'images/placeholder.png'}) center center`, ...styles.projectImage }}></div>
+        <div className="title" style={styles.projectTitle}>
+          {title}
+        </div>
+      </Link>
+    );
+  }
+}
 
 const styles = {
   projectCard: {
@@ -21,7 +43,11 @@ const styles = {
     borderRadius: 2,
     background: '#ffffff',
     boxShadow: '0 2px 5px 0 rgba(0,0,0,.16),0 2px 10px 0 rgba(0,0,0,.12)',
-    textDecoration: 'none'
+    textDecoration: 'none',
+    transition: 'all 0.5s ease'
+  },
+  projectCardHover: {
+    boxShadow: '0 2px 10px 0 rgba(0,0,0,.24),0 2px 20px 0 rgba(0,0,0,.2)'
   },
   projectImage: {
     backgroundSize: 'cover',
